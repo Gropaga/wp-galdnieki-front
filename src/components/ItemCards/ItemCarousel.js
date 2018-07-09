521a99590d2764ef1f98624751db356b3caf051b
Maksims Vorobjovs <ma@ksi.ms>
2018-07-10 01:04:30 +0300

    Implemented carousel

--- src/components/ItemCards/ItemCarousel.js
+++ src/components/ItemCards/ItemCarousel.js
@@ -1,36 +1,29 @@
 import React from 'react';
-import { Carousel, CarouselItem, CarouselControl } from 'reactstrap';
+import { UncontrolledCarousel } from 'reactstrap';
 
 const ItemCarousel = ({ color, colorSelect }) => {
-    if (typeof color[colorSelect] !== 'undefined' && typeof color[colorSelect].gallery[0] !== 'undefined') {
-        const slides = color[colorSelect].gallery.map((image) => {
-            return (
-                <CarouselItem
-                    onExiting={() => alert("onExiting")}
-                    onExited={() => alert("onExited")}
-                    key={image.medium_large[0]}
-                >
-                    <img className="d-block w-100" src={image.medium_large[0]} />
-                </CarouselItem>
-            );
-        });
-
+    return color.map((color, index) => {
         return (
-            <Carousel
-                activeIndex={0}
-                next={ () => alert('next')}
-                previous={() => alert('previous')}
-                interval={ 0 }
-                autoPlay={ false }
+            <div
+                key={ index }
+                className={ index === colorSelect ? '' : 'd-none' }
             >
-                {slides}
-                <CarouselControl direction="prev" directionText="Previous" onClickHandler={ () => alert("previousHandler") } />
-                <CarouselControl direction="next" directionText="Next" onClickHandler={ () => alert("nextHandler") } />
-            </Carousel>
+                <UncontrolledCarousel
+                    interval={ 0 }
+                    autoPlay={ false }
+                    items={ getImages(color) }
+                />
+            </div>
         );
-    } else {
-        return <div/>;
-    }
+    });
 };
+
+const getImages = (color) => color.gallery.map((currentImage) => {
+    return {
+        src: currentImage.medium_large[0],
+        caption: '',
+        altText: '',
+    }
+});
 
 export default ItemCarousel;
